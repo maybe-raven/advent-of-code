72fc3307aea71245b242e98cb7b7ea7e01d1ec33
Raven <dsy1998@live.com>
2023-07-04 01:23:21 -0400

    add: day 7.
    It works but I can see that it's pretty inefficient.
    I'm calling `total_size()` multiple times on the same nodes, and it's
    walking the entire sub-tree every time to calculate the total size.
    This is definitely a dynamic programming situation.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod calorie_counting;
 mod camp_cleanup;
 mod full_of_hot_air;
 mod monkey_map;
+mod no_space_left_on_device;
 mod rock_paper_scissor;
 mod rucksack_reorganization;
 mod supply_stacks;
@@ -10,7 +11,7 @@ mod tuning_trouble;
 mod unstable_difusion;
 
 fn main() -> Result<(), String> {
-    tuning_trouble::main()?;
+    no_space_left_on_device::main()?;
 
     Ok(())
 }
---
+++ src/no_space_left_on_device.rs
@@ -0,0 +1,276 @@
+#![allow(dead_code)]
+use std::{
+    cell::RefCell,
+    fmt::{Display, Formatter},
+    io::{self, Read},
+    ops::Deref,
+    rc::{Rc, Weak},
+};
+
+enum Command<'a> {
+    CD(&'a str),
+    LS(LsResult<'a>),
+}
+
+impl<'a> TryFrom<&'a str> for Command<'a> {
+    type Error = String;
+
+    fn try_from(s: &'a str) -> Result<Self, Self::Error> {
+        let ferrstr = || format!("{} is not a valid command.", s);
+        let (command_str, command_result) = s.split_once('\n').ok_or_else(ferrstr)?;
+        let mut s = command_str.split_whitespace();
+        match s.next().ok_or_else(ferrstr)? {
+            "cd" => Ok(Self::CD(s.next().ok_or_else(ferrstr)?)),
+            "ls" => Ok(Self::LS(LsResult::try_from(command_result)?)),
+            _ => Err(ferrstr()),
+        }
+    }
+}
+
+type DirectoryRef<'a> = Rc<RefCell<Directory<'a>>>;
+type DirectoryWeak<'a> = Weak<RefCell<Directory<'a>>>;
+
+struct Directory<'a> {
+    size: usize,
+    name: &'a str,
+    parent: Option<DirectoryWeak<'a>>,
+    subdirs: Vec<DirectoryRef<'a>>,
+}
+
+impl<'a> Directory<'a> {
+    fn root() -> Self {
+        Self {
+            size: 0,
+            name: "/",
+            parent: None,
+            subdirs: Vec::new(),
+        }
+    }
+
+    fn new(name: &'a str, parent: DirectoryWeak<'a>) -> Self {
+        Self {
+            size: 0,
+            name,
+            parent: Some(parent),
+            subdirs: Vec::new(),
+        }
+    }
+
+    fn has_subdir(&mut self, dir_name: &str) -> bool {
+        self.subdirs.iter().any(|dir| dir.borrow().name == dir_name)
+    }
+
+    fn total_size(&self) -> usize {
+        self.subdirs
+            .iter()
+            .map(|x| x.borrow().total_size())
+            .sum::<usize>()
+            + self.size
+    }
+
+    fn get_answer(&self, mut current_best: usize, target: usize) -> usize {
+        let choose = |size, current_best| {
+            if target < size && size < current_best {
+                size
+            } else {
+                current_best
+            }
+        };
+
+        current_best = choose(self.total_size(), current_best);
+
+        for subdir in &self.subdirs {
+            current_best = choose(
+                subdir.borrow().get_answer(current_best, target),
+                current_best,
+            );
+        }
+
+        current_best
+    }
+}
+
+impl<'a> Display for Directory<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        fn aux<'a>(dir: impl Deref<Target = Directory<'a>>, depth: usize, out: &mut Vec<String>) {
+            out.push(format!(
+                "{space:width$}- dir {} {} | {}",
+                dir.name,
+                dir.size,
+                dir.total_size(),
+                space = ' ',
+                width = depth
+            ));
+
+            for subdir in &dir.subdirs {
+                aux(subdir.borrow(), depth + 2, out);
+            }
+        }
+
+        let mut strings = Vec::new();
+        aux(self, 0, &mut strings);
+        f.write_str(strings.join("\n").as_str())
+    }
+}
+
+struct WorkingDirectory<'a> {
+    root: DirectoryRef<'a>,
+    cwd: DirectoryRef<'a>,
+}
+
+impl<'a> WorkingDirectory<'a> {
+    fn new() -> Self {
+        let root = Rc::new(RefCell::new(Directory::root()));
+        Self {
+            root: root.clone(),
+            cwd: root.clone(),
+        }
+    }
+
+    fn add_child(&mut self, dir_name: &'a str) {
+        let mut cwd = self.cwd.borrow_mut();
+        if !cwd.has_subdir(dir_name) {
+            cwd.subdirs.push(Rc::new(RefCell::new(Directory::new(
+                dir_name,
+                Rc::downgrade(&self.cwd),
+            ))));
+        }
+    }
+
+    fn update(&mut self, ls_result: &LsResult<'a>) {
+        self.cwd.borrow_mut().size = ls_result.size;
+
+        for &dir_name in &ls_result.subdirs {
+            self.add_child(dir_name)
+        }
+    }
+
+    fn cd_root(&mut self) {
+        self.cwd = self.root.clone();
+    }
+
+    fn cd_parent(&mut self) {
+        let Some(parent) = self.cwd.borrow().parent.clone() else { return; };
+        self.cwd = parent
+            .upgrade()
+            .expect("we own the root node so this should always be valid as long as we're valid.");
+    }
+
+    fn cd_dir(&mut self, dir_name: &str) -> Result<(), String> {
+        let Some(new_cwd) = self
+            .cwd
+            .borrow()
+            .subdirs
+            .iter()
+            .find(|x| x.borrow().name == dir_name)
+            .cloned()
+        else {
+            return Err(format!("directory {} does not contain sub-directory {}", self.cwd.borrow().name, dir_name));
+        };
+
+        self.cwd = new_cwd;
+        Ok(())
+    }
+
+    fn execute(&mut self, command: Command<'a>) -> Result<(), String> {
+        match command {
+            Command::CD("/") => {
+                self.cd_root();
+                Ok(())
+            }
+            Command::CD("..") => {
+                self.cd_parent();
+                Ok(())
+            }
+            Command::CD(dir_name) => self.cd_dir(dir_name),
+            Command::LS(ls_result) => {
+                self.update(&ls_result);
+                Ok(())
+            }
+        }
+    }
+
+    fn get_answer(&self) -> usize {
+        const TOTAL_DISK_SPACE: usize = 70000000;
+        const REQUIRED_SPACE: usize = 30000000;
+
+        let root = self.root.borrow();
+        let free_space = TOTAL_DISK_SPACE - root.total_size();
+        root.get_answer(REQUIRED_SPACE, REQUIRED_SPACE - free_space)
+    }
+}
+
+impl Display for WorkingDirectory<'_> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.write_str("file structure:\n")?;
+        writeln!(f, "{}", self.root.borrow())?;
+        f.write_str("current directory: ")?;
+        f.write_str(self.cwd.borrow().name)
+    }
+}
+
+struct LsResult<'a> {
+    size: usize,
+    subdirs: Vec<&'a str>,
+}
+
+impl<'a> LsResult<'a> {
+    fn new() -> Self {
+        Self {
+            size: 0,
+            subdirs: Vec::new(),
+        }
+    }
+
+    fn process_line(&mut self, line: &'a str) -> Option<()> {
+        let mut iter = line.split_whitespace();
+        let head = iter.next()?;
+        if head == "dir" {
+            self.subdirs.push(iter.next()?);
+            Some(())
+        } else if let Ok(item_size) = head.parse::<usize>() {
+            self.size += item_size;
+            Some(())
+        } else {
+            None
+        }
+    }
+}
+
+impl<'a> TryFrom<&'a str> for LsResult<'a> {
+    type Error = String;
+
+    fn try_from(s: &'a str) -> Result<Self, Self::Error> {
+        let mut ls_result = LsResult::new();
+
+        for line in s.lines() {
+            if ls_result.process_line(line).is_none() {
+                return Err(format!(
+                    "malformed `ls` result: \"{}\": failed to parse line: \"{}\"",
+                    s, line
+                ));
+            }
+        }
+
+        Ok(ls_result)
+    }
+}
+
+pub fn main() -> Result<(), String> {
+    let mut working_directory = WorkingDirectory::new();
+
+    let mut input = String::new();
+    io::stdin()
+        .read_to_string(&mut input)
+        .map_err(|e| e.to_string())?;
+
+    for s in input.split('$').skip_while(|s| s.is_empty()) {
+        let command = Command::try_from(s)?;
+        working_directory.execute(command)?;
+    }
+
+    println!("{}", working_directory);
+    println!("{}", working_directory.get_answer());
+
+    Ok(())
+}
