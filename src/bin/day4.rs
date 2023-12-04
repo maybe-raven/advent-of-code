0dde2caad3f92d4832c0412e68a4a6b2d34a8b57
Raven <sd5356@rit.edu>
2023-12-04 04:30:44 -0500

    Optimize the line processing by reusing a Vec.
    Instead of allocating and deallocation a new collection for each line of input,
    we instead just reuse a vec through the entire program.
    However, since we're dropping each line after it's processed,
    in order to push substrings of the input string into the vec,
    we need to use unsafe to cast the str borrow into an arbitrary lifetime.

--- src/bin/day4.rs
+++ src/bin/day4.rs
@@ -1,12 +1,17 @@
-use std::collections::BTreeSet;
-
-fn process_line(s: &str) -> Result<usize, String> {
-    let aux = || {
+fn process_line(s: &str, set: &mut Vec<&str>) -> Result<usize, String> {
+    let mut aux = || {
         let (_, s) = s.split_once(':')?;
         let (a, b) = s.split_once('|')?;
 
-        let a: BTreeSet<&str> = a.split_whitespace().collect();
-        let n = b.split_whitespace().filter(|&x| a.contains(x)).count();
+        // We can use an unsafe block to reborrow `a`, whcih is a substring of `s`, which means
+        // they share a lifetime. This is done because we want to push subsrings of `a` into `set`,
+        // which we want to keep a longer lifetime than the lifetime of `s`, which is one line of
+        // input. We do this to avoid having to do an allocation and deallocation for each line.
+        // This is OK because we make sure to clear the vec before the end of this closure.
+        let a = unsafe { &*(a as *const str) };
+        set.extend(a.split_whitespace());
+        let n = b.split_whitespace().filter(|x| set.contains(x)).count();
+        set.clear();
 
         Some(n)
     };
@@ -43,10 +48,11 @@ impl Counter {
 
 fn solutionate(iter: impl Iterator<Item = Result<String, impl ToString>>) -> Result<usize, String> {
     let mut counter = Counter::default();
+    let mut set = Vec::new();
 
     iter.map(|line| {
         let s = line.map_err(|e| e.to_string())?;
-        let n_matches = process_line(s.as_str())?;
+        let n_matches = process_line(s.as_str(), &mut set)?;
         let n_copies = counter.get() + 1;
         counter.push(n_matches, n_copies);
         Ok(n_copies)
@@ -66,33 +72,37 @@ mod tests {
 
     #[test]
     fn test_process_line_0() {
+        let mut v = Vec::new();
         assert_eq!(
             Ok(4),
-            process_line("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53")
+            process_line("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53", &mut v)
         );
     }
 
     #[test]
     fn test_process_line_1() {
+        let mut v = Vec::new();
         assert_eq!(
             Ok(2),
-            process_line("Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19")
+            process_line("Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19", &mut v)
         );
     }
 
     #[test]
     fn test_process_line_2() {
+        let mut v = Vec::new();
         assert_eq!(
             Ok(1),
-            process_line("Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83")
+            process_line("Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83", &mut v)
         );
     }
 
     #[test]
     fn test_process_line_3() {
+        let mut v = Vec::new();
         assert_eq!(
             Ok(0),
-            process_line("Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36")
+            process_line("Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36", &mut v)
         );
     }
 
