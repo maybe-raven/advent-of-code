09f21b6cca29da42a3a1720a418798783f1f24d6
Raven <sd5356@rit.edu>
2024-09-28 17:19:59 -0400

    I don't remember what this is.

--- lib/adventofelixir.ex
+++ lib/adventofelixir.ex
@@ -12,7 +12,29 @@ defmodule Adventofelixir do
       :world
 
   """
-  def hello do
-    :world
-  end
+  def match_digit(""), do: :done
+  def match_digit(<<d, rest::binary>>) when d in ?0..?9, do: {:ok, d - ?0, rest}
+  def match_digit("zero" <> rest), do: {:ok, 0, rest}
+  def match_digit("one" <> rest), do: {:ok, 1, rest}
+  def match_digit("two" <> rest), do: {:ok, 2, rest}
+  def match_digit("three" <> rest), do: {:ok, 3, rest}
+  def match_digit("four" <> rest), do: {:ok, 4, rest}
+  def match_digit("five" <> rest), do: {:ok, 5, rest}
+  def match_digit("six" <> rest), do: {:ok, 6, rest}
+  def match_digit("seven" <> rest), do: {:ok, 7, rest}
+  def match_digit("eight" <> rest), do: {:ok, 8, rest}
+  def match_digit("nine" <> rest), do: {:ok, 9, rest}
+  def match_digit(<<_, rest::binary>>), do: {:none, rest}
+
+  def process_line(:done, first_digit, last_digit)
+      when is_integer(first_digit) and is_integer(last_digit),
+      do: first_digit * 10 + last_digit
+
+  def process_line(:done, first_digit)
+      when is_integer(first_digit),
+      do: first_digit * 10 + first_digit
+
+  def process_line(:done, first_digit)
+      when is_integer(first_digit),
+      do: first_digit * 10 + first_digit
 end
